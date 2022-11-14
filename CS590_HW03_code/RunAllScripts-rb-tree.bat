31cc97f2ee621b65e020b78520fcce685f63a1b3
sanjeet-jain <sanjeetjain241210@gmail.com>
2022-11-14 14:57:14 -0500

    fixes

--- CS590_HW03_code/RunAllScripts-rb-tree.bat
+++ CS590_HW03_code/RunAllScripts-rb-tree.bat
@@ -1,14 +1,13 @@
 @echo OFF
-set name="bs-tree-random"
+set name="rb-tree-random"
     cd output-%name%
     wt --title %name% bash script-%name%.sh
 cd ./..
-set name="bs-tree-sorted-ascending"
+set name="rb-tree-sorted-ascending"
     cd output-%name%
     wt --title %name% bash script-%name%.sh
-
 cd ./..
-set name="bs-tree-sorted-descending"
+set name="rb-tree-sorted-descending"
     cd output-%name%
     wt --title %name% bash script-%name%.sh
 cd ./..
--- CS590_HW03_code/scripts/RunAllScripts-bs-tree.bat
+++ CS590_HW03_code/scripts/RunAllScripts-bs-tree.bat
@@ -2,11 +2,13 @@
 set name="bs-tree-random"
     cd output-%name%
     wt --title %name% bash script-%name%.sh
-
+cd ./..
 set name="bs-tree-sorted-ascending"
     cd output-%name%
     wt --title %name% bash script-%name%.sh
 
+cd ./..
 set name="bs-tree-sorted-descending"
     cd output-%name%
     wt --title %name% bash script-%name%.sh
+cd ./..
